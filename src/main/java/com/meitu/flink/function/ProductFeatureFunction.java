1fd6b4f1971f359105708ebadaf3bd6852dbc73d
ldl <kiss_summer@126.com>
2025-08-08 17:31:19 +0800

    添加hbase配置

--- src/main/java/com/meitu/flink/function/ProductFeatureFunction.java
+++ src/main/java/com/meitu/flink/function/ProductFeatureFunction.java
@@ -1,10 +1,18 @@
 package com.meitu.flink.function;
 
+import cn.hutool.json.JSONUtil;
+import com.meitu.domain.LogEntity;
 import org.apache.flink.api.common.functions.MapFunction;
 
 public class ProductFeatureFunction implements MapFunction<String,String> {
     @Override
     public String map(String s) throws Exception {
+        try {
+            LogEntity logEntity = JSONUtil.toBean(s, LogEntity.class);
+            return logEntity.getAction();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
